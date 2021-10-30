fe64aac96874a3fdd98c8cdd73abc882ad3e4af6
Dawg4321 <ryan.wiebe@outlook.com>
2021-10-30 13:01:18 +0100

    implemented print out table for DM Cache, general readability improvements

--- DMCache.cpp
+++ DMCache.cpp
@@ -1,52 +1,93 @@
 #include "DMCache.h"
 
+// ~~~Constructor and Destructors~~~
+
 DMCache::DMCache(){ // DMCache constructor
 
     // initialise all values in cache to invalid
     // this must be done before cache can be used hence it is done upon object creation
-    for(int i =0; i < sizeof(cache_entries)/sizeof(cache_entries[0]); i++)
-        cache_entries[i].invalid = true;
+    invalidateCache();
 }
 
-DMCache::~DMCache(){ // DM Cache destructor
+DMCache::~DMCache(){ // DMCache destructor
 }
 
-// ~~~ DMCache::DirectMap ~~~
-//
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~
-char DMCache::getByte(int addr, bool *cache_hit, char input_bytes[4]){
+// ~~~DMCache Member Functions~~~
 
-    char output_to_cpu; // return variable 
+// ~~getByte~~
+//
+// ~~~~~~~~~~~
+char DMCache::getByte(int addr, char input_bytes[4]){
+
+    char output_to_cpu; // return variable for byte from cache
 
     int byte_no = addr&0x3; // gathering byte number (last 2 bits in addr) with "and" operation 
     int set_no = (addr&0x0000FFFC)>>2; // gathering set number (14 bits after byte num in addr) using "and" and shift 2 bits to right (set_no>2)
-    int upper_tag_no = (addr&0xFFFF0000)>>16; // and 16 bits
-
+    int upper_tag_no = (addr&0xFFFF0000)>>16; // gathering tag number (16 bits after set number in addr) using "and" and shift 16 bits to the right (tag>16)
+    
+    string print_tag;
     if(cache_entries[set_no].upper_tag == upper_tag_no && !cache_entries[set_no].invalid){
-        // cache hit
+        // cache hit as tag in address matches tag within set number's cache line
+        
+        print_tag = "hit"; // setting print_tag to hit
+                           // this is used when printing getByte request result
         output_to_cpu = cache_entries[set_no].bytes[byte_no]; // set output variable to corresponding byte
-        printf("cache hit\n");
-
-        // returning cache hit to main
-        *cache_hit = true;
+                                                              // set_no = which line within block to access
+                                                              // byte_no = which byte to access from block
+        hit_counter++; // increment hit counter to track this hit
     }
     else{
-        // cache miss
+        // cache miss (matching upper tag was not found within a valid cache entry of corresponding set number)
 
-        // set address to corresponding values
-        cache_entries[set_no].upper_tag = upper_tag_no;
-        cache_entries[set_no].invalid = false;
+        print_tag = "miss"; // setting print_tag to miss
+                            // this is used when printing getByte request result
+        cache_entries[set_no].upper_tag = upper_tag_no; // set cache line to corresponding values
+        cache_entries[set_no].invalid = false;          // set_no = which line within block to access
 
-        // copy bytes into address
-        for(int i = 0; i < 4; i++)
-            cache_entries[set_no].bytes[i] = input_bytes[i];   
-        printf("cache miss\n");
-        
-        // returning cache miss to main
-        *cache_hit = false;
-        // set output variable to 0 as cac
-        output_to_cpu =  0;
+        for(int i = 0; i < 4; i++) // iterate through bytes to load
+            cache_entries[set_no].bytes[i] = input_bytes[i];  // copy bytes into address 
+                                                              // set_no = which line within block to access
+
+        output_to_cpu = cache_entries[set_no].bytes[byte_no]; // setting return variable to corresponding byte
+                                                              // set_no = which line within block to access
+                                                              // byte_no = which byte to access from block
+        miss_counter++; // increment miss counter to track this miss
     }
-    return output_to_cpu;
+
+    if(hit_counter+miss_counter == 1){ // if this is the first time getByte is called, create table headers 
+        string line = "--------------------------------------------------------------"; // line string for printf output to help table format
+
+        printf("Direct Mapped Cache Request Table\n"); // print title
+        // print line seperator
+        printf("+%.3s+%.13s+%.9s+%.9s+%.11s+%.12s+%.4s+%.5s+\n",line.c_str(), line.c_str(), line.c_str(), line.c_str(), line.c_str(), line.c_str(), line.c_str(), line.c_str());
+        // print title line of table
+        printf("| # |Address (hex)|tag (hex)|set (hex)|byte# (hex)| hit/miss |hit#|miss#|\n"); 
+        // print line sperator
+        printf("+%.3s+%.13s+%.9s+%.9s+%.11s+%.12s+%.4s+%.5s+\n",line.c_str(), line.c_str(), line.c_str(), line.c_str(), line.c_str(), line.c_str(), line.c_str(), line.c_str());
+    }
+    // print details regarding cache 
+    printf("|%3d|   %08x  |   %04x  |   %04x  |     %1x     |Cache %-4s|%4d|%5d|\n",hit_counter + miss_counter, // total number of cache queries
+                                                                                    addr, // address requested
+                                                                                    upper_tag_no, // address tag
+                                                                                    set_no, // address set number
+                                                                                    byte_no, // byte offset
+                                                                                    print_tag.c_str(), // string which prints hit or miss
+                                                                                    hit_counter, // number of hits
+                                                                                    miss_counter // number of misses
+                                                                                    );
+
+    return output_to_cpu; // return byte from specified cache location
+}
+
+// ~~invalidateCache~~
+// 
+// ~~~~~~~~~~~~~~~~~~~
+void DMCache::invalidateCache(){
+    
+    hit_counter = 0; // reseting hit counter
+    miss_counter = 0; // reseting miss counter
+
+    for(int i = 0; i < sizeof(cache_entries)/sizeof(cache_entries[0]); i++) // iterate through each line within cache
+        cache_entries[i].invalid = true; // invalidate cache line
+    return;
 }
--- DMCache.h
+++ DMCache.h
@@ -2,12 +2,11 @@
 #define DMCACHE_H
 
 #include <stdio.h>
-#include <stdbool.h>
+#include <string>
 
 using namespace std;
 
-struct DMCacheLine{
-    int LRU; 
+struct DMCacheEntry{
     bool invalid;
     unsigned short upper_tag; 
     char bytes[4];
@@ -16,11 +15,18 @@ struct DMCacheLine{
 class DMCache
 {
     public:
+        // contructor and destructor
         DMCache();
         ~DMCache();
-        char getByte(int x, bool *cache_hit, char input_bytes[4]);
+        // member functions
+        void invalidateCache(); // function to invalidate all entries within cache 
+        char getByte(int x, char input_bytes[4]); // function to query cache for a specific address value
+                                                  // if cache miss occurs, load bytes from input array
     private:
-        struct DMCacheLine cache_entries[16384];
+        struct DMCacheEntry cache_entries[16384]; // Array representing a cache block
+                                                  // Each entry represents a cache line.
+        int hit_counter; // counter to track number of cache hits 
+        int miss_counter; // counter to track number of cache misses
 };
 
 #endif
